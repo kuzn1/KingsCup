f6afb5bd5f420cd15d48e24d01f9838aca7e2b29
256791 <256791@student.pwr.edu.pl>
2021-05-15 18:59:26 +0200

    Options update + some minor changes

--- app/src/main/java/pwr/am/kingscup/OptionsActivity.kt
+++ app/src/main/java/pwr/am/kingscup/OptionsActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import pwr.am.kingscup.databinding.ActivityOptionsBinding
 
 class OptionsActivity : Activity() {
@@ -17,7 +18,31 @@ class OptionsActivity : Activity() {
         setContentView(binding.root)
 
         config = getSharedPreferences("KingsCupConfig", MODE_PRIVATE)
+
+        initItems()
+
         loadConfig()
+    }
+
+    private fun initItems() {
+        ArrayAdapter.createFromResource(
+                this,
+                R.array.card_texture_name_array,
+                android.R.layout.simple_spinner_item
+            ).also{ adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                binding.textureSpinner.adapter = adapter
+            }
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.texture_quality_name_array,
+            android.R.layout.simple_spinner_item
+        ).also{ adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.qualitySpinner.adapter = adapter
+        }
+
     }
 
     override fun onPause() {
@@ -31,14 +56,68 @@ class OptionsActivity : Activity() {
     }
 
     private fun loadConfig() {
+        //Nickname
         binding.nickInput.setText(config.getString("nick", "Player"))
+
+        //Gender
+        if(config.getString("gender", "male")=="male")
+            onGenderCheck(binding.maleCheckBox)
+        else
+            onGenderCheck(binding.femaleCheckBox)
+
+        //Texture
+        binding.textureSpinner.setSelection(
+            resources.getTextArray(R.array.card_texture_name_array)
+                .indexOf(config.getString("texture", "Default")))
+
+        //Quality
+        binding.qualitySpinner.setSelection(
+            resources.getTextArray(R.array.texture_quality_name_array)
+                .indexOf(config.getString("quality", "Low")))
+
     }
 
     private fun saveConfig() {
         with(config.edit()){
+            //Nickname
             if(binding.nickInput.text.toString().isNotEmpty())
                 this.putString("nick",binding.nickInput.text.toString())
+
+            //Gender
+            if(binding.maleCheckBox.isChecked)
+                this.putString("gender", "male")
+            else
+                this.putString("gender", "female")
+
+            //Texture
+            this.putString("texture",
+                binding.textureSpinner.selectedItem as String
+            )
+
+            //Quality
+            this.putString("quality",
+                binding.qualitySpinner.selectedItem as String
+            )
+            val index = resources.getTextArray(R.array.texture_quality_name_array)
+                .indexOf(config.getString("quality", "Low"))
+
+            this.putInt("card_height", resources.getIntArray(R.array.card_height)[index])
+            this.putInt("card_width", resources.getIntArray(R.array.card_width)[index])
+
             this.apply()
+        }
+    }
+
+    fun onGenderCheck(view: View) {
+        when (view) {
+            binding.femaleCheckBox -> {
+                binding.femaleCheckBox.isChecked = true
+                binding.maleCheckBox.isChecked = false
+            }
+            binding.maleCheckBox -> {
+                binding.maleCheckBox.isChecked = true
+                binding.femaleCheckBox.isChecked = false
+            }
         }
     }
 }
