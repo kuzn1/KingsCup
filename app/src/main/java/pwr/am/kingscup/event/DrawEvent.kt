340cc55495f50741b558f3f5df568596dcb17985
256791 <256791@student.pwr.edu.pl>
2021-06-03 21:56:05 +0200

    Synchronization commit

--- app/src/main/java/pwr/am/kingscup/event/DrawEvent.kt
+++ app/src/main/java/pwr/am/kingscup/event/DrawEvent.kt
@@ -1,9 +1,14 @@
 package pwr.am.kingscup.event
 
+import android.speech.tts.TextToSpeech
+import pwr.am.kingscup.R
 import pwr.am.kingscup.services.GameClient
 import pwr.am.kingscup.render.Animation
+import java.util.*
 
-class DrawEvent(game: GameClient) : Event(game) {
+class DrawEvent(game: GameClient) : Event(game) , TextToSpeech.OnInitListener{
+    private var textToSpeech  = TextToSpeech(game.context, this)
+    private var tts = false
     private var index = 0
 
     fun setIndex(i : Int){
@@ -22,7 +27,14 @@ class DrawEvent(game: GameClient) : Event(game) {
         if(index == 0) index = game.drawables.lastIndex
         game.drawables[index].animate(
             Animation(0.0f,0.0f,-2.0f, 0.0f,180.0f,0.0f,1000).also {
-                it.after { game.respond("draw_event_done", true) }
+                it.after {
+                    game.respond("draw_event_done", true)
+                    if(tts){
+                        val cardNames = game.context.resources.getStringArray(R.array.card_name)
+                        val text = cardNames[game.drawables[index].id%13]
+                        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null,"")
+                    }
+                }
             }
         )
     }
@@ -31,5 +43,12 @@ class DrawEvent(game: GameClient) : Event(game) {
         game.drawables[index].animate(
             Animation(0.0f,0.0f,-1.8f, 0.0f,0.0f,0.0f,100)
         )
+    }
+
+    override fun onInit(status: Int) {
+        if(status != TextToSpeech.ERROR){
+            tts = true
+            textToSpeech.language = Locale.ITALIAN
+        }
     }
 }
